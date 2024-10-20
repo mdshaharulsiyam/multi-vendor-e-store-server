a85db1c67b6c292a209ffaff18854414639be9da
shaharulsiyam <shaharulsiyam0273@gmail.com>
2024-10-20 16:19:30 +0600

    update urls

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:3001", 'http://localhost:3000',],
+    origin: ["http://localhost:3001", 'http://localhost:3000', 'https://multi-vendor-fronted.vercel.app'],
     credentials: true,
   })
 );
