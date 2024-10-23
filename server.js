48dbb6ed3a63f86fb64bc9ac226d9ea27c322704
shaharulsiyam <shaharulsiyam0273@gmail.com>
2024-10-23 11:33:58 +0600

    fix issues

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ connectDatabase();
 
 // create server
 const server = app.listen(process.env.PORT, () => {
-  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+  console.log(`Server is running on https://multi-vendor-fronted.vercel.app:${process.env.PORT}`);
 });
 
 // middlewares
